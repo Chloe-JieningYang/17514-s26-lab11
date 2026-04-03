1f1dddb1bcf9823fa2d48f7abb0f2c1279748143
Jiening Yang <jieningy@andrew.cmu.edu>
2026-04-03 01:23:38 -0400

    implemented undo

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -65,6 +65,12 @@ class App extends React.Component<Props, GameState> {
     }
   }
 
+  undo = async () => {
+    const response = await fetch('/undo');
+    const json = await response.json();
+    this.setState({ cells: json['cells'], instructions: json['instructions'] });
+  }
+
   createCell(cell: Cell, index: number): React.ReactNode {
     if (cell.playable)
       /**
@@ -121,8 +127,7 @@ class App extends React.Component<Props, GameState> {
         </div>
         <div id="bottombar">
           <button onClick={this.newGame}>New Game</button>
-          {/* Exercise: implement Undo function */}
-          <button>Undo</button>
+          <button onClick={this.undo}>Undo</button>
         </div>
       </div>
     );
